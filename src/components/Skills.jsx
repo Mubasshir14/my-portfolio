72dc3baf340c1b513fe5c8447300cebae2c2443e
Mubasshir Al Kasshaf <mubasshiralkasshaf22@gmail.com>
2024-09-07 18:04:29 +0600

    Skill Sectio Edited

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,37 +3,53 @@ import css from '../assets/css.png';
 import javascript from '../assets/javascript.png';
 import tailwind from '../assets/tailwind.png';
 import react from '../assets/react.png';
+import node from '../assets/node.png';
+import expressjs from '../assets/expressjs.png';
+import mongodb from '../assets/mongodb.png';
+
+const skillsData = [
+  { name: 'HTML', image: html },
+  { name: 'CSS', image: css },
+  { name: 'Javascript', image: javascript },
+  { name: 'Tailwind', image: tailwind },
+  { name: 'React', image: react },
+  { name: 'Node', image: node },
+  { name: 'Express', image: expressjs },
+  { name: 'MongoDB', image: mongodb }
+];
 
 const Skills = () => {
   return (
-    <div id='skill' className='bg-black border border-gray-600 text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-3 place-items-center md:flex md:justify-between md:items-center md:mt-20 md:mb-20'>
+    <div className='my-10'>
+        <div className="pb-8 text-center">
+                <p className='text-4xl mb-3 font-bold primary-color'>My Tech Skills</p>
+               
+            </div>
+      <div className='hidden md:flex flex-wrap justify-center items-center max-w-[1200px] mx-auto'>
+        {skillsData.map((skill, index) => (
+          <div key={index} className="max-w-xs border-2 p-4 m-4 rounded-md shadow-md bg-black">
+            <img src={skill.image} alt={skill.name} className="object-cover object-center w-full rounded-t-md h-20 md:h-36" />
+            <div className="flex flex-col justify-between pt-2">
+              <h2 className="text-2xl text-center text-white font-semibold">{skill.name}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+{/* mobile */}
+      <div className='md:hidden flex flex-wrap justify-center items-center max-w-[1200px] mx-auto'>
+        {skillsData.map((skill, index) => (
+          <div key={index} className="max-w-xs border-2 p-2 m-2 rounded-md shadow-md bg-black">
+            <img src={skill.image} alt={skill.name} className="object-cover object-center w-full rounded-t-md h-20 " />
+            <div className="flex flex-col justify-between pt-2">
+              <h2 className="text-md text-center text-white font-semibold">{skill.name}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
 
-
-        <h2 className='text-gray-700 text-xl md:text-4xl font-bold m-4'>My <br /> Teach <br /> Skill</h2>
-
-
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px] '>
-            <img src={html} alt="" className='hover:scale-105 hover:transform  hover:translate-all' />
-            <p className='mt-2'>HTML</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={css} alt="" width={100} height={100} className='hover:scale-105 hover:transform  hover:translate-all' />
-            <p className='mt-2'>CSS</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={javascript} alt="" width={100} height={100} className='hover:scale-105 hover:transform  hover:translate-all' />
-            <p className='mt-2'>JavascriptL</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={tailwind} alt="" width={100} height={100} className='hover:scale-105 hover:transform  hover:translate-all' />
-            <p className='mt-2'>Tailwind</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={react} alt="" width={100} height={100} className='hover:scale-105 hover:transform  hover:translate-all'/>
-            <p className='mt-2'>React</p>
-        </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
